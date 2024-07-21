92cf531c776ece6b3a2cc7fd75841df29fec8ef8
personalleandrocesar <leandrocesar.goo@gmail.com>
2024-07-21 01:50:35 -0300

    updt

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 // app.config.ts
 export default defineAppConfig({
     nuxtIcon: {
-        size: '21px', // default <Icon> size applied
+        size: '16px', // default <Icon> size applied
         class: 'icon', // default <Icon> class applied
         aliases: {
             // 'nuxt': 'logos:nuxt-icon',
